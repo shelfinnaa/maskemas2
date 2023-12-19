78bbe5455aa1ba8f86cd5386e72f2ec19a2282a0
DaC00KIE <119905464+DaC00KIE@users.noreply.github.com>
2023-12-19 14:39:14 +0700

    done create for feedback i think

--- database/seeders/OrderStatusSeeder.php
+++ database/seeders/OrderStatusSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\OrderStatus;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,29 @@ class OrderStatusSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $status1 = OrderStatus::create([
+            'name' => 'In discussion'
+        ]);
+
+        $status2 = OrderStatus::create([
+            'name' => 'Payment Process'
+        ]);
+
+        $status3 = OrderStatus::create([
+            'name' => 'Packing'
+        ]);
+
+        $status4 = OrderStatus::create([
+            'name' => 'On-Route'
+        ]);
+
+        $status5 = OrderStatus::create([
+            'name' => 'Delivered'
+        ]);
+
+        $status6 = OrderStatus::create([
+            'name' => 'Received'
+        ]);
+
     }
 }
--- resources/views/feedback/create.blade.php
+++ resources/views/feedback/create.blade.php
@@ -1,33 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Add Feedback</title>
-</head>
-<body>
+@include('admin.adminnavigation')
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <h1>Adding Feedback</h1>
-    <form method="POST" action="{{ route('feedback.store') }}">
+    <form method="POST" action="">
 
-        <div>
+        <div class="form-group">
             <label for="client">Select Client</label>
             <select name="client" id="client">
                 <option value="" disabled selected>Select a Client</option>
                 @if($clients->count() > 0)
-                <option value="">THERE ARE USERS</option>
+                @foreach ($clients as $client)
+                    <option value="<?=$client->id?>"><?=$client->name?></option>
+                @endforeach
                 @else
-                <option value="">THERE ARE NO USERS</option>
+                <option value="" disabled>THERE ARE NO USERS</option>
                 @endif
             </select>
         </div>
 
-        <div>
+        <div class="form-group">
             <label>Client Name</label>
             <input type="text" name="name" id="name" placeholder="Name">
         </div>
 
-        <div>
+        <div class="form-group">
             <label>Client Job Title</label>
             <input type="text" name="title" id="title" placeholder="E.g. Founder">
         </div>
