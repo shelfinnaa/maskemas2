5587ea6989ce25556b82fda3ad53d99b63a92ac9
shelfinnaa <potatoessquid@gmail.com>
2024-01-08 10:33:28 +0700

    user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,22 +15,41 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            'name' => 'dummy',
-            'password' => Hash::make('1234'),
-            'email' => 'email@email.com',
-            'usertype' => 'user'
-        ]);
-        DB::table('users')->insert([
-            'name' => 'dummyadmin',
-            'password' => Hash::make('1234'),
-            'email' => 'email@email.net',
-            'usertype' => 'admin'
-        ]);
-        DB::table('users')->insert([
             'name' => 'admin',
             'password' => Hash::make('admin123'),
             'email' => 'admin@gmail.com',
             'usertype' => 'admin'
         ]);
+        DB::table('users')->insert([
+            'name' => 'IndoTech Solutions',
+            'password' => Hash::make('indotech123'),
+            'email' => 'Indotechsolution@gmail.com',
+            'usertype' => 'user'
+        ]);
+        DB::table('users')->insert([
+            'name' => 'BaliCraft Innovations',
+            'password' => Hash::make('balicraft123'),
+            'email' => 'Balicraftinnovation@gmail.com',
+            'usertype' => 'user'
+        ]);
+        DB::table('users')->insert([
+            'name' => 'JavaTech Ventures',
+            'password' => Hash::make('javatech123'),
+            'email' => 'javatechventures@gmail.com',
+            'usertype' => 'user'
+        ]);
+        DB::table('users')->insert([
+            'name' => 'CelebesGreen',
+            'password' => Hash::make('celebesgreen123'),
+            'email' => 'celebesgreen@gmail.com',
+            'usertype' => 'user'
+        ]);
+        DB::table('users')->insert([
+            'name' => 'KopiKreasi',
+            'password' => Hash::make('kopikreasi123'),
+            'email' => 'kopikreasi@gmail.com',
+            'usertype' => 'user'
+        ]);
+
     }
 }
